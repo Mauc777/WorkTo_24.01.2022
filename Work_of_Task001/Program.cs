79328243f563914d1f736d56f42fff9da6cb5ad7
Маис <Kozannostra777@mail.ru>
2022-01-25 15:21:32 +0300

    Добавление блока-метода Selection для сортировки нужных элементов с заполнением нового массива ArrayFinite

--- Work_of_Task001/Program.cs
+++ Work_of_Task001/Program.cs
@@ -7,4 +7,22 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
 Console.Clear();
-string[] ArrayStrings = {"Hello", "2", "world", ":-)","ghgh","hg"};
+string[] ArrayStrings = { "Hello", "2", "world", ":-)", "ghgh", "hg" };
+
+string[] Selection(string[] ArrayStrings)
+{
+    string[] ArrayFinite = new string[0];
+    int j = 0;
+    for (int i = 0; i < ArrayStrings.Length; i++)
+    {
+        if (ArrayStrings[i].Length <= 3)
+        {
+            int lenght = ArrayFinite.Length;
+            Array.Resize(ref ArrayFinite, lenght + 1);
+            ArrayFinite[j] = ArrayStrings[i];
+            j++;
+
+        }
+    }
+    return (ArrayFinite);
+}
